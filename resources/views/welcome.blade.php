7474b8504f80b3a93b4c24ba6246311a4b14c4f2
William Montesedoca <wvmon360@gmail.com>
2017-03-22 19:47:46 -0600

    Removed ! sign

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    EliteTrainingPro Coming Soon!
+                    EliteTrainingPro Coming Soon
                 </div>
 
                 <div class="links">
