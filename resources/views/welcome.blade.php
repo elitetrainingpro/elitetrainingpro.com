28bff189d92b5db19ba944d2c2a1f500147a1143
William Montesedoca <wvmon360@gmail.com>
2017-03-22 19:40:39 -0600

    Fixed Typo and do not include lock file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    EliteTraingPro Coming Soon!
+                    EliteTrainingPro Coming Soon!
                 </div>
 
                 <div class="links">
