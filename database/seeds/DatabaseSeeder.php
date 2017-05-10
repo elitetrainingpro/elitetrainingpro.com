33e9ca622809dc1b60cb9f98950435e8d4c94e95
hjcoder18 <hjmarshall18@gmail.com>
2017-05-09 23:13:48 -0600

    added seed files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$this->call(GoalTypesTableSeeder::class);
+    	$this->call(NoteTypesTableSeeder::class);
+    	$this->call(RolesTableSeeder::class);
+    	$this->call(CoachRoleTypesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
