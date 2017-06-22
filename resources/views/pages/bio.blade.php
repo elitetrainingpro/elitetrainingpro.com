dd308fd8e06084b267d4aa580ad87fc9a52e6cb7
jaortiz9 <ortiz.janine@gmail.com>
2017-06-22 15:33:44 -0400

    Fixed header for bio page Issue#86

--- resources/views/pages/bio.blade.php
+++ resources/views/pages/bio.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.default')
+@extends('layouts.app')
 
 @section('title', '| Bio')
 
 @section('stylesheets')
-	<link media="all" type="text/css" rel="stylesheet" href="{{ URL::asset('assets/css/header.css') }}"></link>
+	<link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('assets/css/homie.css') }}" rel="stylesheet">
 @endsection
 
 @section('scripts')
@@ -18,84 +19,112 @@
     </script>
 @endsection
 
+@section('navbar')
+	<nav class="navbar navbar-default navbar-static-top">
+	    <div class="container">
+	        <div class="navbar-header">
+	            <!-- Branding Image -->
+	            <div class="icon-style"><img src="{{ URL::asset('assets/images/elitetrainingpro.png') }}" alt="No image found" height="45px" width="45px">
+	            	<a class="navbar-brand">
+	               		{{ config('app.name', 'Elite Training Pro') }}
+	            	</a>
+	            </div>
+	        </div>
+	    </div>
+	</nav>
+@endsection
+
 @section('content')
 <div class="container">
 	<div class="row">
-		{!! Form::open(['route' => 'bios.store', 'data-parsley-validate' => '', 'files' => true]) !!}
-			{{ Form::label('image', 'Upload Image (Less than 2 MB):') }}
-			{{ Form::file('image') }}
-		    {{ Form::label('city', 'City') }}
-		    {{ Form::text('city', null, ['class' => 'form-control', 'required' => '', 'maxlength' => '191']) }}
-		    
-		    {{ Form::label('state', 'State') }}
-		    {{ Form::select('state',[
-			    'States' => [
-			    				null => 'Please Select',
-								'AL' => 'Alabama',
-								'AK' => 'Alaska',
-								'AZ' => 'Arizona',
-								'AR' => 'Arkansas',
-								'CA' => 'California',
-								'CO' => 'Colorado',
-								'CT' => 'Connecticut',
-								'DE' => 'Delaware',
-								'DC' => 'District Of Columbia',
-								'FL' => 'Florida',
-								'GA' => 'Georgia',
-								'HI' => 'Hawaii',
-								'ID' => 'Idaho',
-								'IL' => 'Illinois',
-								'IN' => 'Indiana',
-								'IA' => 'Iowa',
-								'KS' => 'Kansas',
-								'KY' => 'Kentucky',
-								'LA' => 'Louisiana',
-								'ME' => 'Maine',
-								'MD' => 'Maryland',
-								'MA' => 'Massachusetts',
-								'MI' => 'Michigan',
-								'MN' => 'Minnesota',
-								'MS' => 'Mississippi',
-								'MO' => 'Missouri',
-								'MT' => 'Montana',
-								'NE' => 'Nebraska',
-								'NV' => 'Nevada',
-								'NH' => 'New Hampshire',
-								'NJ' => 'New Jersey',
-								'NM' => 'New Mexico',
-								'NY' => 'New York',
-								'NC' => 'North Carolina',
-								'ND' => 'North Dakota',
-								'OH' => 'Ohio',
-								'OK' => 'Oklahoma',
-								'OR' => 'Oregon',
-								'PA' => 'Pennsylvania',
-								'RI' => 'Rhode Island',
-								'SC' => 'South Carolina',
-								'SD' => 'South Dakota',
-								'TN' => 'Tennessee',
-								'TX' => 'Texas',
-								'UT' => 'Utah',
-								'VT' => 'Vermont',
-								'VA' => 'Virginia',
-								'WA' => 'Washington',
-								'WV' => 'West Virginia',
-								'WI' => 'Wisconsin',
-								'WY' => 'Wyoming',
-							],
-			], null, ['required']) }}<br/>
-			{{ Form::label('bio', 'Bio') }}
-			{{ Form::textarea('bio', null, ['class' => 'form-control', 'required' => '', 'maxlength' => '191']) }}
-			{{ Form::label('identity', 'Role') }}
-			{{ Form::select('identity',[
-			    'Identity' => [
-			    				null => 'Please Select',
-								'Athlete' => 'Athlete',
-								'Coach' => 'Coach',
-							],
-			], null, ['required']) }}
-			{{ Form::submit('Submit') }}
-		{!! Form::close() !!}
+		<div class="col-md-8  col-md-offset-2">
+			<h3>Tell Us About Yourself</h3>
+			<hr>
+				{!! Form::open(['route' => 'bios.store', 'data-parsley-validate' => '', 'files' => true]) !!}
+
+				{{ Form::label('image', 'Upload Image (Less than 2 MB):') }}
+				{{ Form::file('image') }} <br/>
+
+				{{ Form::label('bio', 'Bio') }}
+				{{ Form::textarea('bio', null, ['class' => 'form-control', 'required' => '', 'maxlength' => '191']) }} <br/>
+
+			    {{ Form::label('city', 'City') }}
+			    {{ Form::text('city', null, ['class' => 'form-control', 'required' => '', 'maxlength' => '191']) }}<br/>
+			    <div class="col-sm-6">
+			    {{ Form::label('state', 'State') }}
+			    {{ Form::select('state',[
+				    'States' => [
+				    				null => 'Please Select',
+									'AL' => 'Alabama',
+									'AK' => 'Alaska',
+									'AZ' => 'Arizona',
+									'AR' => 'Arkansas',
+									'CA' => 'California',
+									'CO' => 'Colorado',
+									'CT' => 'Connecticut',
+									'DE' => 'Delaware',
+									'DC' => 'District Of Columbia',
+									'FL' => 'Florida',
+									'GA' => 'Georgia',
+									'HI' => 'Hawaii',
+									'ID' => 'Idaho',
+									'IL' => 'Illinois',
+									'IN' => 'Indiana',
+									'IA' => 'Iowa',
+									'KS' => 'Kansas',
+									'KY' => 'Kentucky',
+									'LA' => 'Louisiana',
+									'ME' => 'Maine',
+									'MD' => 'Maryland',
+									'MA' => 'Massachusetts',
+									'MI' => 'Michigan',
+									'MN' => 'Minnesota',
+									'MS' => 'Mississippi',
+									'MO' => 'Missouri',
+									'MT' => 'Montana',
+									'NE' => 'Nebraska',
+									'NV' => 'Nevada',
+									'NH' => 'New Hampshire',
+									'NJ' => 'New Jersey',
+									'NM' => 'New Mexico',
+									'NY' => 'New York',
+									'NC' => 'North Carolina',
+									'ND' => 'North Dakota',
+									'OH' => 'Ohio',
+									'OK' => 'Oklahoma',
+									'OR' => 'Oregon',
+									'PA' => 'Pennsylvania',
+									'RI' => 'Rhode Island',
+									'SC' => 'South Carolina',
+									'SD' => 'South Dakota',
+									'TN' => 'Tennessee',
+									'TX' => 'Texas',
+									'UT' => 'Utah',
+									'VT' => 'Vermont',
+									'VA' => 'Virginia',
+									'WA' => 'Washington',
+									'WV' => 'West Virginia',
+									'WI' => 'Wisconsin',
+									'WY' => 'Wyoming',
+								],
+				], null, ['required']) }}
+				</div>
+				
+				<div class="col-sm-6"">
+				{{ Form::label('identity', 'Role') }}
+				{{ Form::select('identity',[
+				    'Identity' => [
+				    				null => 'Please Select',
+									'Athlete' => 'Athlete',
+									'Coach' => 'Coach',
+								],
+				], null, ['required']) }}
+				<br/><br/>
+				</div>
+				{{ Form::submit('Submit', ['class' => 'btn btn-primary btn-lg btn-block']) }}
+				<br/>
+			{!! Form::close() !!}
+		</div>
 	</div>
 </div>
 @endsection
