3c5c1edfc455aed80c44fe440624f7426266a5ad
jaortiz9 <ortiz.janine@gmail.com>
2017-06-30 15:35:17 -0400

    Coach HomePage can now view their athletes #101

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,19 +27,38 @@ class HomeController extends Controller
     {
     	$bio = DB::table('bios')->where('email', Auth::user()->email)->first();
 
-		// Check to see if there was a row returned from the database.
-    	if ($bio != NULL) {
-    		// Check to see if the identity column is not null
-	    	if ($bio->identity != NULL) {
-	    		if ($bio->identity == 'Coach'){
-	    			return view('pages.home')->with('bio', $bio);
-	    		} else {
-	    			return view('pages.athletes-home')->with('bio', $bio);
-	    		}
-	    	}else { // If identity is null then go to bio page (This should never ever happen)
-	    		return view('pages.bio');
+
+        if ($bio->identity != NULL) {
+            if ($bio->identity == 'Coach'){
+
+                // get list of athletes that the coach has.
+                $athleteToCoaches = DB::table('athlete_to_coaches')->where('coach_id', $bio->user_id)->get();
+
+                //declare an array to sotre athlete's bio information
+                $athletes = array();
+
+                // Get all the athlete's bio information and Athlete's Name
+                foreach ($athleteToCoaches as $athleteToCoach){
+                    //getting Athlete Bio
+                    $athleteBio = DB::table('bios')->where('user_id', $athleteToCoach->athlete_id)->get();
+                    // Getting Athlete's Name
+                    $athleteName = DB::table('users')->where('id', $athleteToCoach->athlete_id)->get();
+                    // Merging them into one
+                    $athlete = $athleteBio->merge($athleteName);
+                    array_push($athletes, $athlete); 
+                }
+
+                // data
+                $data = array(
+                    'bio' => $bio,
+                    'athletes' => $athletes
+                );
+
+                return view('pages.home')->with($data);
+            } else {
+                return view('pages.athletes-home')->with('bio',$bio);
 	    	}
-        } else {
+        } else { // If identity is null then go to bio page (This should never ever happen)
         	return view('pages.bio');
         }
     }
